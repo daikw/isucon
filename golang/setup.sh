b2bf986d5af7a326ee115f9d9552c1c02dd43edf
daikw <daikw@yak-shaver.com>
2022-07-18 15:51:12 +0900

    :wrench:

--- golang/setup.sh
+++ golang/setup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+# isu-go
 make
 chown isucon app
+systemctl restart isu-go
 
+# nginx
 systemctl reload nginx
 mv /var/log/nginx/access.log /var/log/nginx/access.log.$(date +%T)
 nginx -s reopen
 
+# mysql
 systemctl restart mysql
 rm /var/log/mysql/mysql-slow.log
 mysqladmin flush-logs
 
-systemctl restart isu-go
+# redis
+# ...
 
 echo "Application restart done, run your benchmarker ..."
 
 sleep 5
 sudo query-digester
 
-ls /tmp/slow_query_*.digest | xargs -IXXX sudo mv XXX /home/isucon/digests/$1
+filename=/home/isucon/digests/$1
+ls /tmp/slow_query_*.digest | xargs -IXXX sudo mv XXX $filename
+echo "Query digest saved as: " $filename
